8656543d5e7ba51ffb8ab088f1e4a58a10e83eae
iranilson1 <64569275+iranilson1@users.noreply.github.com>
2023-05-16 08:18:13 -0300

    Update Marca_.py
    adicionado a pesquisa e algumas mudanças no codigo

--- Marca_.py
+++ Marca_.py
@@ -1,50 +1,111 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.contrib.auth.decorators import login_required
+
 from .models import *
 
 
-def cadastrarPessoas(request):
-	if(request.method == 'POST'):
-		nomes = request.POST['nome']
-		pessoa = Pessoas(nome=nomes)
-		pessoa.save()
 
-	#contexto
-	mensagem = 'Teste'
-	alert = 'alert-success'
-	listaPessoas = Pessoas.objects.all()
-
-	context={'mensagem':mensagem, 'alert':alert, 'listaPessoas':listaPessoas}
-	render(request, 'pessoas.html', context)
-
-
-def alterarPessoas(request, id=None):
-	if(request.method =='POST'):
-		nome = request.POST['nome']
-		pessoa = Pessoas.objects.get(id=id)
-		pessoa.nome = nome
-		pessoa.save()
-
-	#contexto
-	mensagem = 'Teste'
-	alert = 'alert-success'
-	listaPessoas = Pessoas.objects.all()
-
-	context={'mensagem':mensagem, 'alert':alert, 'listaPessoas':listaPessoas}
-	render(request, 'pessoas.html', context)
-
-
-def deletarPessoas(request, id=None):
+@login_required(redirect_field_name='',login_url="index")
+def pessoas(request):
 	if(request.method == 'GET'):
-		pessoa = Pessoas.objects.get(id=id)
-		pessoa.delete()
+		listaPessoa = Pessoas.objects.all().order_by('nome')
+		context = {"listaPessoa":listapessoa}
+		return render(request, 'pessoas.html', context)
+	else:
+		return render(request, 'nao-autorizado.html')
 
-	#contexto
-	mensagem = 'Teste'
-	alert = 'alert-success'
-	listaPessoas = Pessoas.objects.all()
 
-	context={'mensagem':mensagem, 'alert':alert, 'listaPessoas':listaPessoas}
-	render(request, 'pessoas.html', context)
+@login_required(redirect_field_name='',login_url="index")
+def cadastrarPessoa(request):
+	if(request.method == 'POST'):
+		mensagem = ''
+		alert = 'alert-success'
+		try:
+			nome = request.POST['nome']
+			pessoa = Pessoas(nome=nome)
+			pessoa.save()
+			mensagem = 'Pessoa "%s" foi cadastrada com sucesso.'%(nome)
+
+		except Exception as e:
+			mensagem = 'Erro ao cadastrar Pessoa. Consulte o responsável e informe: "%s"'%(str(e))
+			alert = 'alert-danger'
+
+		listaPessoa = Pessoas.objects.all()
+		context={'mensagem':mensagem, 'alert':alert, 'listaPessoa':listaPessoa}
+		return render(request, 'cadastraPessoa.html', context)
+	
+	elif(request.method == 'GET'):
+		return render(request, 'cadastraPessoa.html')
+	
+	else:
+		return render(request, 'nao-autorizado.html')
+
+
+@login_required(redirect_field_name='',login_url="index")
+def alterarPessoa(request, codigo=None):
+	if(request.method =='POST'):
+		mensagem = ''
+		alert = 'alert-success'
+		try:
+			nome = request.POST['nome']
+			pessoa = Pessoas.objects.get(id=int(codigo))
+			nomeAntigo = pesssoa.nome
+			pessoa.nome = nome
+			pessoa.save()
+			mensagem = 'Pessoa "%s" foi alterada com sucesso para "%s".'%(nomeAntigo, nome)
+		except Exception as e:
+			mensagem = 'Erro ao alterar Pessoa. Consulte o responsável e informe: "%s"'%(str(e))
+			alert = 'alert-danger'
+
+		listaPessoa = Pessoa.objects.all().order_by('nome')
+		context={'mensagem':mensagem, 'alert':alert, 'listaPessoa':listaPessoa}
+		return render(request,'pessoa.html',context)
+	elif(request.method == 'GET'):
+		try:
+			pessoa=Pessoas.objects.get(id=int(codigo))
+			context={'pessoa':pessoa}
+			return render(request, 'alteraPessoa.html', context=context)
+		except Exception as e:
+			mensagem = 'Erro ao alterar pessoa. Consulte o responsável e informe: "%s"'%(str(e))
+			alert = 'alert-danger'
+			listaPessoa = Pessoa.objects.all().order_by('nome')
+
+			context={'mensagem':mensagem, 'alert':alert, 'listaPessoa':listaPessoa}
+			return render(request,'pessoas.html',context)
+	else:
+		return render(request, 'nao-autorizado.html')
+
+
+@login_required(redirect_field_name='',login_url="index")
+def deletarpessoa(request, codigo=None):
+	if(request.method == 'GET'):
+		mensagem = ''
+		alert = 'alert-success'
+		try:
+			pessoa = Pesoas.objects.get(id=int(codigo))
+			pessoa.delete()
+			mensagem = 'pessoa "%s" foi deletada com sucesso.'%(pessoa.nome)
+		
+		except Exception as e:
+			mensagem = 'Erro ao deletar pessoa. Consulte o responsável e informe: "%s"'%(str(e))
+			alert = 'alert-danger'
+
+		listaPessoa = pessoas.objects.all().order_by('nome')
+		context={'mensagem':mensagem, 'alert':alert,'listaPessoa':listaPessoa}
+		return render(request,'pessoa.html',context)
+	else:
+		return render(request, 'nao-autorizado.html')
+
+
+@login_required(redirect_field_name='',login_url='index')
+def pesquisarPessoas(request):
+	if(request.method=='GET'):
+		filtro = request.GET.get('filtro', None)
+		pessoas = [str(x) for x in Pessoas.objects.filter(nome__icontains = filtro).order_by('nome')]
+		data = {'pessoas':pessoas}
+		return JsonResponse(data)
+
+
 
 
